adc44f03b2a5b6955ebac74b0124d6930c31b429
Надія Ткач <nadiya77723@gmail.com>
2022-11-28 21:23:50 +0200

    доробила сторінку MovieDetails

--- src/pages/MovieDetails/MovieDetails.jsx
+++ src/pages/MovieDetails/MovieDetails.jsx
@@ -1,14 +1,51 @@
-import { useEffect, useState } from "react";
-import { useParams, useLocation, Outlet } from "react-router-dom";
-
+import { useEffect, useState } from 'react';
+import { useParams, useLocation, Outlet } from 'react-router-dom';
+import { getMovieDetails } from 'services/api';
+import { Suspense } from 'react';
 
 const MovieDetails = () => {
-    const [movie, setMovie] = useState(null);
-    const { movieId } = useParams();
-    const location = useLocation();
+  const [movie, setMovie] = useState(null);
+  const { movieId } = useParams();
+  const location = useLocation();
 
-    useEffect(() => {
-        
-    })
-}
+  useEffect(() => {
+    getMovieDetails(movieId)
+      .then(setMovie)
+      .catch(error => console.log(error));
+  }, [movieId]);
 
+  const from = location.state?.from ?? '/';
+
+  return (
+    <>
+      {movie && (
+        <Container>
+          <LinkButton to={from}>Go back</LinkButton>
+          <MovieInfo info={movie}></MovieInfo>
+          <AddInfo>
+            <AddInfoTitle> Additional information</AddInfoTitle>
+            <AddInfoWrapper>
+              <ul>
+                <AddInfoItem>
+                  <AddInfoItemLink to="cast" state={{ from }}>
+                    Cast
+                  </AddInfoItemLink>
+                </AddInfoItem>
+                <AddInfoItem>
+                  <AddInfoItemLink to="reviews" state={{ from }}>
+                    Reviews
+                  </AddInfoItemLink>
+                </AddInfoItem>
+              </ul>
+            </AddInfoWrapper>
+            <Suspense fallback={<div>Loading...</div>}>
+              <Outlet />
+            </Suspense>
+          </AddInfo>
+        </Container>
+      )}
+    </>
+  );
+};
+
+export default MovieDetails;
